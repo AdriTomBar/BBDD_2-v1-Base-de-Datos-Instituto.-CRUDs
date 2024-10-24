0e84d54ded009f74f453a71310d4bb8e85891369
AdriTomBar <145601607+esopom@users.noreply.github.com>
2024-10-24 13:57:09 +0200

    Implementacion ServicioCurso Hecha

---
+++ src/Services/ServicioCurso.java
@@ -0,0 +1,72 @@
+package Services;
+
+import data.DaoCurso;
+import data.models.Curso;
+
+public class ServicioCurso implements Servicio<Curso,String>{
+
+    @Override
+    public void create(Curso objeto) {
+        if (validarIdCurso(objeto) && evalidarDB(objeto) == true) {
+            DaoCurso daoCurso = new DaoCurso();
+            daoCurso.create(objeto);
+        } else {
+            System.out.println("No se pudo crear el curso");
+        }
+    }
+
+    @Override
+    public Curso read(String identificador) {
+        if (identificador.length() > 0 && (evalidarDB(identificador)==false)) {
+            DaoCurso daoCurso = new DaoCurso();
+            return daoCurso.read(identificador);
+        }
+        return null;
+    }
+
+    @Override
+    public void update(Curso objeto) {
+        if (validarIdCurso(objeto) && evalidarDB(objeto) == false) {
+            DaoCurso daoCurso = new DaoCurso();
+            daoCurso.update(objeto);
+        } else {
+            System.out.println("No se pudo actualizar el curso");
+        }
+    }
+
+    @Override
+    public void delete(String identificador) {
+        if (identificador.length() > 0 && (evalidarDB(identificador)==false)) {
+            DaoCurso daoCurso = new DaoCurso();
+            daoCurso.delete(identificador);
+        } else {
+            System.out.println("No se pudo eliminar el curso");
+        }
+    }
+
+    private Boolean evalidarDB(Curso curso){
+        DaoCurso daoCurso = new DaoCurso();
+        if (daoCurso.read(curso.getCod_curso()) != null) {
+            throw new IllegalArgumentException("El curso ya existe en la base de datos");
+        }
+        return false;
+    }
+
+    private Boolean evalidarDB(String curso){
+        DaoCurso daoCurso = new DaoCurso();
+        if (daoCurso.read(curso) != null) {
+            throw new IllegalArgumentException("El curso ya existe en la base de datos");
+        }
+        return false;
+    }
+
+    private Boolean validarIdCurso(Curso curso){
+        if (curso.getCod_curso().length() < 1) {
+            throw new IllegalArgumentException("El id del curso debe tener al menos 1 caracter");
+        }
+        return true;
+    }
+
+   
+    
+}
